d15b7066fdb30fea3b8d5a667b42f3bccfdbf92b
Bartosz Sofinski <bartosz.sofinski@swmansion.com>
2025-07-23 10:32:04 +0200

    RN 0.76, New arch, Reanimated 3.18

--- metro.config.js
+++ metro.config.js
@@ -14,9 +14,6 @@ const { assetExts, sourceExts } = defaultConfig.resolver;
  * @type {import('metro-config').MetroConfig}
  */
 const config = {
-  transformer: {
-    babelTransformerPath: require.resolve('react-native-svg-transformer'),
-  },
   resolver: {
     assetExts: assetExts.filter((ext) => ext !== 'svg'),
     sourceExts: [...sourceExts, 'svg'],
